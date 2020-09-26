d65a38765b533fde809d94d94ab11f05ab1ce5eb
maxim.zaytsev <maxim.zaytsev@aurea.com>
2020-09-26 08:05:42 +0300

    MANAGERS_24737

--- tr-jira-missmatch/update-tc-statuses.py
+++ tr-jira-missmatch/update-tc-statuses.py
@@ -6,7 +6,7 @@ client = APIClient('https://testrail.devfactory.com/')
 client.user = os.environ['AD_USER']
 client.password = os.environ['AD_PASSWORD']
 
-with open('test-cases.csv', 'r') as f:
+with open('file-with-test-cases-to-update.csv', 'r') as f:
     lines = f.readlines()
     i = 1
     for line in lines:
@@ -15,5 +15,5 @@ with open('test-cases.csv', 'r') as f:
         to_status = line.split(',')[2].replace("\n","")
         print("[{0} of {1}] updating {2} to {4}".format(i, len(lines) + 1, id, from_status, to_status))
         result = client.send_post("update_case/{0}".format(id), { 'custom_tc_status': to_status, 'comment': 'Updating status based on linked JIRA issue status' })
-        sleep(0.1)
+        #sleep(0.01)
         i = i + 1
